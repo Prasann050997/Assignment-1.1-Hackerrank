c2820955ceb87574b05eaeae09deed441275809c
Patil Prasann <124381560+Prasann050997@users.noreply.github.com>
2023-04-18 23:58:04 +0530

    Add files via upload

---
+++ Assignment-1.1-Hackerrank.py
@@ -0,0 +1,48 @@
+#Find and read 20 interview questions for Data Types, Operators, Conditional Statements, Looping Statements, Functions.
+
+# DATA TYPES
+# Q-1 Why are these data types known as Python’s core data types?
+# ANS They are known as the core data types because they are part of the Python language itself and are always available to create other objects, you usually need to call functions in imported modules.
+# Q-2 What does immutable mean and what three types of Python core data types are considered immutable?
+# ANS An immutable data type is a type of object which cannot be modified after its creation. Numbers, strings, and tuples in Python fall into this category. Although you cannot modify an immutable object in place, you can always create a new one by running an expression.
+# Q-3 What are Data Types?
+# ANS A data type is a set of values and a set of operations defined on data. An implementation of a data type is an expression of data and operations in terms of a specific programming language such as Java, C ++, or Python.
+# Q-4 What is true about complex numbers?
+# ANS The real and imaginary component can be float
+
+# OPERATORS
+# Q-5 What is the value of the following Python Expression print(36 / 4) ?
+# ANS 9.0
+# Q-6 Bitwise shift operators (<<, >>) has higher precedence than Bitwise And(&) operator.
+# ANS TRUE
+# Q-7 What is the output of print(10 - 4 * 2) ?
+# ANS 2
+# Q-8 What is the output of the following code ? print(bool(0), bool(3.14159), bool(-3), bool(1.0+1j))
+# ANS False True True True
+
+# CONDITIONAL STATEMENTS
+# Q-9 What constitutes “True” in Python?
+# ANS A true expression is any expression that does not evaluate to 0, the empty list [ ], tuple ( ), dictionary { } or the objects None or False.
+# Q-10 What are the three main conditional statements in Python?
+# ANS if, elif, and else
+# Q-11 What does elif mean?
+# ANS It means else if. It is used after an if statement, to do another comparison.
+# Q-12 How is the Python switch statement used?
+# ANS This is a trick question, there is no built-in switch statement in Python, which is unusual. A switch statement can be easily created using if-elif using lambda or with Python dictionaries.
+# Q-13 Name the loops used in Python.
+# ANS for loop and while loop.
+# Q-14 Which loop in Python is a counter-controlled loop?
+# ANS for loop
+# Q-15 What keyword is used to end looping completely?
+# ANS break keyword
+# Q-16 For loops are used to loop over.
+# ANS Iterable objects
+# Q-17 What are the two types of functions in Python?
+# ANS built-in functions and user-defined functions.
+# Q-18 Do Python functions have return values? If yes, then how many can they have?
+# ANS Yes, Python functions can have return values. They can have a single return value, or they can have multiple return values.
+# Q-19 What function can generate a list of numbers?
+# ANS range function
+# Q-20 What is the use of lambda function with filter() in Python?
+# ANS To filter out elements of a list that do not satisfy a certain condition.
+
